4fafebdd252dcd3106bed417351e76df9c3dd5a6
Seddawy0 <seddawy0@gmail.com>
2026-02-17 03:34:14 +0200

    HackerRank: Aritmetic Operators

---
+++ HackerRank/Python Problem Solving/Easy/Arithmetic_Operators_02.py
@@ -0,0 +1,13 @@
+# Problem Link : https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+# Difficulty : Easy
+
+######################## Aritmetic Operators ########################
+a = int(input())
+b= int(input())
+add_num = a + b
+sub_num = a - b
+mul_num = a * b
+print(add_num)
+print(sub_num)
+print(mul_num)
+#####################################################################
