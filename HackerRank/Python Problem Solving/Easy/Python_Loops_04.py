3e48044ae869ca6603f3fefe25ef9e9160028467
Seddawy0 <seddawy0@gmail.com>
2026-02-17 03:53:19 +0200

    HackerRank: Python Loops

---
+++ HackerRank/Python Problem Solving/Easy/Python_Loops_04.py
@@ -0,0 +1,8 @@
+# Problem Link : https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+# Difficulty : Easy
+
+######################## Python Loops ########################
+n = int(input())
+for i in range(n):
+    print(i ** 2)
+##############################################################
