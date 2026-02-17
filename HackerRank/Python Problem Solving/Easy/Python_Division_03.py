78d646a26e27484c9a18f2d8a00b620d75206b30
Seddawy0 <seddawy0@gmail.com>
2026-02-17 03:38:26 +0200

    HackerRank: Python Division

---
+++ HackerRank/Python Problem Solving/Easy/Python_Division_03.py
@@ -0,0 +1,11 @@
+# Problem Link : https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# Difficulty : Easy
+
+######################## Python Division ########################
+a = int(input())
+b = int(input())
+int_div = a // b
+float_div = a / b
+print(int_div)
+print(float_div)
+#################################################################
