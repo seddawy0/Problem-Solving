4649c3b2ae48176faa8645218ec8198c31469df4
Seddawy0 <seddawy0@gmail.com>
2026-02-18 19:16:19 +0200

    HackerRank: Python Print

---
+++ HackerRank/Python Problem Solving/Easy/Python_Print_05.py
@@ -0,0 +1,8 @@
+# Problem Link : https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+# Difficulty : Easy
+
+######################## Python Print ########################
+n = int(input())
+for i in range(1, n+1):
+    print(i, end="")
+#############################################################
