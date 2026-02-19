58ca5f859de2b5e9a9fe90c3bc3699502599989d
Seddawy0 <seddawy0@gmail.com>
2026-02-19 04:30:49 +0200

    CodeWars: Sum Arrays

---
+++ CodeWars/Python Problem Solving/kyu 8/Sum_Arrays_02.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/python
+# Difficulty : KYU 8
+
+######################## Sum Arrays ########################
+def sum_array(a):
+    return sum(a)
+###########################################################
