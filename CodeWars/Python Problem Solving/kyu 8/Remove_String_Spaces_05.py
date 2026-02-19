43eebf30e052078ceb48aade4f1b98b5e36da9e2
Seddawy0 <seddawy0@gmail.com>
2026-02-19 04:39:53 +0200

    CodeWars: Remove String Spaces

---
+++ CodeWars/Python Problem Solving/kyu 8/Remove_String_Spaces_05.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+# Difficulty : KYU 8
+
+######################## Remove String Spaces ########################
+def no_space(x):
+    return x.replace(" ", "")
+######################################################################
