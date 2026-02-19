f9c02d0200dfb200c50ac8694d74553e483c29b4
Seddawy0 <seddawy0@gmail.com>
2026-02-19 04:38:48 +0200

    CodeWars: Third Angle of a Triangle

---
+++ CodeWars/Python Problem Solving/kyu 8/Third_Angle_Of_Triangle_04.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/5a023c426975981341000014/train/python
+# Difficulty : KYU 8
+
+######################## Third Angle of a Triangle ########################
+def other_angle(a, b):
+    return 180 - (a + b)
+###########################################################################
