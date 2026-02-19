c8137fc50d9fb6eeb8f29f2d1d9128cf65a63f18
Seddawy0 <seddawy0@gmail.com>
2026-02-20 01:58:02 +0200

    CodeWars: Personalized Message

---
+++ CodeWars/Python Problem Solving/kyu 8/Personalized_Message_08.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/python
+# Difficulty : KYU 8
+
+######################## Personalized Message ########################
+def greet(name, owner):
+    return 'Hello boss' if name == owner else 'Hello guest'
+######################################################################
