1511912353278a7f14735a0766fea21ad3049673
Seddawy0 <seddawy0@gmail.com>
2026-02-19 03:47:46 +0200

    CodeWars: Reversed String

---
+++ CodeWars/Python Problem Solving/kyu 8/Reversed_String_01.py
@@ -0,0 +1,8 @@
+# Problem Link : https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+# Difficulty : KYU 8
+
+######################## Reversed String ########################
+def solution(string):
+    return string[::-1]
+print(solution("world"))
+#################################################################
