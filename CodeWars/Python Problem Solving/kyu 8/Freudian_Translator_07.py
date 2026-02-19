6fd31c77c684c2858ad7db23169514f9bde64594
Seddawy0 <seddawy0@gmail.com>
2026-02-20 01:50:57 +0200

    CodeWars: Freudian Translator

---
+++ CodeWars/Python Problem Solving/kyu 8/Freudian_Translator_07.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/5713bc89c82eff33c60009f7/train/python
+# Difficulty : KYU 8
+
+######################## Freudian Translator ########################
+def to_freud(sentence):
+    return " ".join("sex" for word in sentence.split())
+#####################################################################
