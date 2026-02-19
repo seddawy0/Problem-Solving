d45ad18bfc25938e757207847c4bdbe794e55a75
Seddawy0 <seddawy0@gmail.com>
2026-02-19 04:33:09 +0200

    CodeWars: Basic Variable Assignment

---
+++ CodeWars/Python Problem Solving/kyu 8/Basic_Variable_Assignment_03.py
@@ -0,0 +1,12 @@
+# Problem Link : https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/python
+# Difficulty : KYU 8
+
+######################## Basic Variable Assignment ########################
+# a == "code"
+# b == "wa.rs"
+# name == a + b
+
+a = "code"
+b = "wa.rs"
+name = a + b
+###########################################################################
