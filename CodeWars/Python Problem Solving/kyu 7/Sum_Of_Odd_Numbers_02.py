00601297dd0ac1316a4a2cb957ce4865050c4a35
Seddawy0 <seddawy0@gmail.com>
2026-02-20 22:13:37 +0200

    CodeWars: Sum Of Odd Numbers

---
+++ CodeWars/Python Problem Solving/kyu 7/Sum_Of_Odd_Numbers_02.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/python
+# Difficulty : KYU 7
+
+######################## Sum Of Odd Numbers ########################
+def row_sum_odd_numbers(n):
+    return n**3
+###################################################################
