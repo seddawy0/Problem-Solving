bb4f460c8fce04e4998db737d464de13d8480ad2
Seddawy0 <seddawy0@gmail.com>
2026-02-21 00:42:11 +0200

    CodeWars: Perimeter Sequence

---
+++ CodeWars/Python Problem Solving/kyu 7/Perimeter_Sequence_03.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/589519d1f0902e01af000054/train/python
+# Difficulty : KYU 7
+
+######################## Perimeter Sequence ########################
+def perimeter_sequence(a, n): 
+    return (4 * a) * n
+###################################################################
