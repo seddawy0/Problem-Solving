44441cab1a7a0ec3aae9d181612aad8075eb2d97
Seddawy0 <seddawy0@gmail.com>
2026-02-24 19:00:53 +0200

    CodeWars: Quadratic Cofficient Solver

---
+++ CodeWars/Python Problem Solving/kyu 8/13_Quadratic_Cofficients_Solver.py
@@ -0,0 +1,8 @@
+# Problem Link : https://www.codewars.com/kata/5d59576768ba810001f1f8d6/train/python
+# Difficulty : KYU 8
+
+######################## Quadratic Cofficient Solver ########################
+def quadratic(x1, x2):
+    a, b, c = 1, -(x1+x2), x1 * x2
+    return a, b, c
+#############################################################################
