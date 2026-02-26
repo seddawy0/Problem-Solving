7ac8e257d2c267195451f717b51e20c9596187e0
Seddawy0 <seddawy0@gmail.com>
2026-02-26 06:07:04 +0200

    HackerRank: List Comprehension

---
+++ HackerRank/Python Problem Solving/Easy/15_List_Comprehension.py
@@ -0,0 +1,11 @@
+# Problem Link : https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true
+# Difficulty : Easy
+
+######################## List Comprehension ########################
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+posible_coordinates = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
+print(posible_coordinates)
+####################################################################
