3bb93071c26b04f7e296697fb9960ac2810a939a
Seddawy0 <seddawy0@gmail.com>
2026-02-28 08:35:34 +0200

    CodeWars: Sort Numbers

---
+++ CodeWars/Python Problem Solving/kyu 7/15_Sort_Numbers.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/python
+# Difficulty : KYU 7
+
+######################## Sort Numbers ########################
+def solution(nums):
+    return sorted(nums) if not nums == None else []
+##############################################################
