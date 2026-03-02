d24cff0534659f2f1b99175d70fef45f4391cde1
Seddawy0 <seddawy0@gmail.com>
2026-03-02 09:18:04 +0200

    CodeWars: Number Of Decimal Digits

---
+++ CodeWars/Python Problem Solving/kyu 7/18_Number_Of_Decimal_Digits.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/58fa273ca6d84c158e000052/train/python
+# Difficulty : KYU 7
+
+######################## Number Of Decimal Digits ########################
+def digits(n):
+    return len(str(n))
+##########################################################################
