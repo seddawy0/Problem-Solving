11a3672706f7689f39c18f97dd8c9c208e31a947
Seddawy0 <seddawy0@gmail.com>
2026-04-02 23:01:14 +0200

    CodeWars: SQL Make Upper Case

---
+++ CodeWars/SQL/kyu 8/2_Make_Upper_Case.sql
@@ -0,0 +1,11 @@
+--# Problem Link : https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/sql
+--# Difficulty : KYU 8
+
+--######################## Third Angle of a Triangle ########################
+--# write your SQL statement here: you are given a table 'makeuppercase' with column 's',
+--# return a table with column 's' and your uppercased result in a column named 'res'.
+SELECT
+  s,
+  UPPER(s) AS res
+FROM makeuppercase
+--###########################################################################
