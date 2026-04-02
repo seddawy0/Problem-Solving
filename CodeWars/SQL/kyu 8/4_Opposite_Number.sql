0219ca1db05f769503503ed4d7e76aa524d22c13
Seddawy0 <seddawy0@gmail.com>
2026-04-02 23:36:52 +0200

    CodeWars: SQL Opposite number

---
+++ CodeWars/SQL/kyu 8/4_Opposite_Number.sql
@@ -0,0 +1,9 @@
+--# Problem Link : https://www.codewars.com/kata/56dec885c54a926dcd001095/train/sql
+--# Difficulty : KYU 8
+
+--######################## Opposite number ########################
+-- You will be given a table: "opposite", with a column: "number". Return a table with a column: "res".
+SELECT
+  -number AS res
+FROM opposite
+--#################################################################
