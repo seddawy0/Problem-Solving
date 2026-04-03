cc24ff2facf328859b42eb3e9b04c8cb48e37da1
Seddawy0 <seddawy0@gmail.com>
2026-04-03 20:57:29 +0200

    CodeWars: Python Find The Smallest Integer In The Array

---
+++ CodeWars/Python/kyu 8/14_Smallest_in_Array.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/python
+# Difficulty : KYU 8
+
+######################## Find The Smallest Integer In The Array ########################
+def find_smallest_int(arr):
+    return min(arr)
+########################################################################################
