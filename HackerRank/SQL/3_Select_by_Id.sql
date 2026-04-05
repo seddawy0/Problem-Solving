209eb620ed40b5d45ebc6443ec1c2407ec32929a
Seddawy0 <seddawy0@gmail.com>
2026-04-05 19:32:33 +0200

    HackerRank: SQL Select by Id

---
+++ HackerRank/SQL/3_Select_by_Id.sql
@@ -0,0 +1,9 @@
+--# Problem Link : https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+--# Difficulty : Easy
+
+--######################## Select by Id ########################
+-- Query all columns for a city in CITY with the ID 1661.
+SELECT *
+FROM CITY
+WHERE ID = 1661
+--##############################################################
