8351e87730ae9028426400004d8650d62afdaf18
Seddawy0 <seddawy0@gmail.com>
2026-04-05 21:16:59 +0200

    HackerRank: SQL Weather Observation

---
+++ HackerRank/SQL/5_Weather_Observation_1.sql
@@ -0,0 +1,10 @@
+--# Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+--# Difficulty : Easy
+
+--######################## Weather Observation ########################
+-- Query a list of CITY and STATE from the STATION table.
+SELECT
+    CITY,
+    STATE
+FROM STATION
+--#####################################################################
