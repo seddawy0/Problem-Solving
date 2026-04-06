3e9cfe6ab452d7f3333455170f5a81f64ad526d7
Seddawy0 <seddawy0@gmail.com>
2026-04-06 22:00:41 +0200

    HackerRank: SQL Name of Employees

---
+++ HackerRank/SQL/9_Name_of_Employees.sql
@@ -0,0 +1,10 @@
+--# Problem Link : https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+--# Difficulty : Easy
+
+--######################## Name of Employees ########################
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+SELECT name
+FROM Employee
+ORDER BY
+    name
+--###################################################################
