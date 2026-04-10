53266f7223d965d4da39cc96605ec22469634481
Seddawy0 <seddawy0@gmail.com>
2026-04-10 13:29:05 +0200

    HackerRank: SQL Weather Observation 17

---
+++ HackerRank/SQL/32_Weather_Observation_17.sql
@@ -0,0 +1,13 @@
+--# Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-17/problem?isFullScreen=true
+--# Difficulty : Easy
+
+--######################## Weather Observation ########################
+-- Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780.
+-- Round your answer to 4 decimal places.
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE
+    LAT_N > 38.7780
+ORDER BY LAT_N ASC
+LIMIT 1
+--#####################################################################
