7dfeee9e5c7f8034720c1eceaaca25aca059cc5c
Seddawy0 <seddawy0@gmail.com>
2026-04-10 13:20:05 +0200

    HackerRank: SQL Weather Observation 15

---
+++ HackerRank/SQL/30_Weather_Observation_15.sql
@@ -0,0 +1,14 @@
+--# Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true
+--# Difficulty : Easy
+
+--######################## Weather Observation ########################
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345.
+-- Round your answer to 4 decimal places.
+SELECT
+    ROUND(LONG_W, 4)
+FROM STATION
+WHERE
+    LAT_N < 137.2345
+ORDER BY LAT_N DESC
+LIMIT 1
+--#####################################################################
