425fe255312079bb89c7ae448aca391034f90b31
Seddawy0 <seddawy0@gmail.com>
2026-04-14 22:19:21 +0200

    CodeWars: SQL Multiply

---
+++ CodeWars/SQL/kyu 8/15_Multiply.sql
@@ -0,0 +1,8 @@
+--# Problem Link : https://www.codewars.com/kata/50654ddff44f800200000004/train/sql
+--# Difficulty : KYU 8
+
+--######################## Multiply ########################
+--# This code does not execute properly. Try to figure out why.
+-- SELECT price + amount AS total FROM items
+SELECT price * amount AS total FROM items
+--##########################################################
